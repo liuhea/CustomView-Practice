c8c7c0216c5a6f4c3f3eff2e9821c598425f0721
liuhe <liuhe@juran.com.cn>
2018-03-23 01:26:35 +0800

    drawArc and drawText

--- customview/src/main/java/com/liuhe/customviewpractice/CanvasView.kt
+++ customview/src/main/java/com/liuhe/customviewpractice/CanvasView.kt
@@ -1,10 +1,14 @@
 package com.liuhe.customviewpractice
 
 import android.content.Context
-import android.graphics.Canvas
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import com.liuhe.kotlinutilslib.getScreenHeight
+import com.liuhe.kotlinutilslib.getScreenWidth
 import com.liuhe.kotlinutilslib.log
+import com.liuhe.kotlinutilslib.toast
+
 
 /**
  * 自定义View 默认四个构造函数
@@ -17,9 +21,38 @@ import com.liuhe.kotlinutilslib.log
  */
 class CanvasView(context: Context) : View(context) {
 
+    val txt = "刘贺"
+    private lateinit var redPaint: Paint
+    private lateinit var bluePaint: Paint
+    private val facePoint = PointF(getScreenWidth(context) / 2, getScreenHeight(context) / 2)
+    private val pointLeftEyeStart = PointF(420f, 840f)
+    private val pointRightEyeStart = PointF(620f, 840f)
+    private val pointLeftEyeStop = PointF(460f, 840f)
+    private val pointRightEyeStop = PointF(660f, 840f)
+    val faceRadius = 200f
+
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : this(context)
 
-    constructor(context: Context, attrs: AttributeSet) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet) : this(context, attrs, 0) {
+        init()
+    }
+
+    private fun init() {
+        redPaint = Paint()
+        bluePaint = Paint()
+        redPaint.apply {
+            color = Color.RED
+            style = Paint.Style.STROKE
+            // 抗锯齿（毛刺），开启后，占用内存
+            isAntiAlias = true
+        }
+        bluePaint.apply {
+            color = Color.BLUE
+            style = Paint.Style.FILL_AND_STROKE
+            // 抗锯齿（毛刺），开启后，占用内存
+            isAntiAlias = true
+        }
+    }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
@@ -31,8 +64,67 @@ class CanvasView(context: Context) : View(context) {
         "onLayout".log()
     }
 
+    /**
+     * 绘制阶段调用此方法
+     * @param canvas 画布
+     */
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         "onDraw".log()
+        /*
+        画圆
+        cx,cy圆心的坐标
+        radius圆的半径
+        paint 画笔
+         */
+        context.toast("facePoint.x=${facePoint.x}，facePoint.y=${facePoint.y}")
+//        canvas?.drawCircle(facePoint.x, facePoint.y, faceRadius, paint)
+
+        /*
+        画线
+        startX,startY
+        stopX,stopY
+        paint
+         */
+        canvas?.drawLine(pointLeftEyeStart.x, pointLeftEyeStart.y, pointLeftEyeStop.x, pointLeftEyeStop.y, redPaint)
+
+        /*
+        RectF oval :　用于确定圆弧形状与尺寸的椭圆边界（即椭圆外切矩形）
+       float startAngle  开始角度（以时钟3点的方向为0°，顺时针为正方向）
+       float sweepAngle 扫过角度（以时钟3点的方向为0°，顺时针为正方向）
+       boolean useCenter 是否包含圆心
+       Paint paint绘制圆弧的画笔
+         */
+        val rectF = RectF(440f, 1060f, 640f, 1160f)
+        canvas?.drawRect(rectF, redPaint)
+
+        canvas?.drawArc(rectF, 30f, 120f, true, bluePaint)
+
+        drawText(canvas)
+
+    }
+
+    private fun drawText(canvas: Canvas?) {
+        val paint = Paint().apply {
+            color=Color.RED
+            //设置防抖动
+            isDither=true
+            //抗锯齿
+            isAntiAlias=true
+            strokeWidth=3f
+            textSize = 100f
+        }
+
+        //测量文本高
+        val textBounds = Rect()
+        paint.getTextBounds(txt, 0, txt.length, textBounds)
+        val textH = textBounds.height()
+
+        paint.style = Paint.Style.STROKE
+        canvas?.drawText(txt, 0f, (100 + textH * 0).toFloat(), paint)
+        paint.style = Paint.Style.FILL
+        canvas?.drawText(txt, 0f, (100 + textH * 1 + 50).toFloat(), paint)
+        paint.style = Paint.Style.FILL_AND_STROKE
+        canvas?.drawText(txt, 0f, (100 + textH * 2 + 100).toFloat(), paint)
     }
 }
--- kotlinutilslib/src/main/java/com/liuhe/kotlinutilslib/Utils.kt
+++ kotlinutilslib/src/main/java/com/liuhe/kotlinutilslib/Utils.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.text.TextUtils
 import android.util.Log
 import android.widget.Toast
+import android.util.DisplayMetrics
+
 
 /**
  * Created by liuhe on 20/03/2018.
@@ -25,3 +27,41 @@ fun String.log(logType: Int = Log.DEBUG) {
         else -> Log.d("liuhe->d", this)
     }
 }
+
+/**
+ *  屏幕宽（像素，如：3200px）
+ */
+fun getScreenWidth(context: Context): Float {
+    val appContext = context.applicationContext
+    var dm = appContext.resources.displayMetrics as DisplayMetrics
+    return dm.widthPixels.toFloat()
+}
+
+/**
+ * 屏幕高（像素，如：1280px）
+ */
+fun getScreenHeight(context: Context): Float {
+    val appContext = context.applicationContext
+    var dm = appContext.resources.displayMetrics as DisplayMetrics
+    return dm.heightPixels.toFloat()
+}
+
+/**
+ * 屏幕密度（每寸像素：120/160/240/320）
+ */
+fun getScreenDPI(context: Context): Float {
+    val appContext = context.applicationContext
+    var dm = appContext.resources.displayMetrics as DisplayMetrics
+    return dm.densityDpi.toFloat()
+}
+
+/**
+ *  屏幕密度（像素比例：0.75/1.0/1.5/2.0）
+ */
+fun getScreenDensity(context: Context): Float {
+    val appContext = context.applicationContext
+    var dm = appContext.resources.displayMetrics as DisplayMetrics
+    return dm.density
+}
+
+
