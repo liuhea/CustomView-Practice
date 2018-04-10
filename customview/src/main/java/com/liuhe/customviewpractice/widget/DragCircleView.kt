73741ecc2150adb49fe4fc5e7c23f3dec816cb90
liuhe <liuhe@juran.com.cn>
2018-04-11 01:13:12 +0800

    up and move ; ValueAnimator

--- customview/src/main/java/com/liuhe/customviewpractice/widget/DragCircleView.kt
+++ customview/src/main/java/com/liuhe/customviewpractice/widget/DragCircleView.kt
@@ -1,10 +1,12 @@
 package com.liuhe.customviewpractice.widget
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
+import android.view.animation.OvershootInterpolator
 import com.liuhe.customviewpractice.utils.GeometryUtil
 import com.liuhe.customviewpractice.utils.getStatusBarHeight
 
@@ -28,10 +30,6 @@ class DragCircleView @JvmOverloads constructor(context: Context, attrs: Attribut
      * 固定圆半径
      */
     private var stableCircleRadius = 20f
-    /**
-     * 固定圆附着点
-     */
-    private var stablePoints = listOf(PointF(200f, 180f), PointF(200f, 220f))
 
     /**
      * 拖拽圆圆心
@@ -41,10 +39,6 @@ class DragCircleView @JvmOverloads constructor(context: Context, attrs: Attribut
      * 拖拽圆半径
      */
     private var dragCircleRadius = 30f
-    /**
-     *拖拽圆附着点
-     */
-    private var dragPoints = listOf(PointF(100f, 70f), PointF(100f, 130f))
 
     /**
      * 贝塞尔曲线参考点
@@ -53,7 +47,7 @@ class DragCircleView @JvmOverloads constructor(context: Context, attrs: Attribut
 
 
     /**
-     * 拖拽圆心最远距离
+     * 拖拽圆移动的最大距离
      */
     private var maxDragDistance = 500f
     /**
@@ -64,7 +58,11 @@ class DragCircleView @JvmOverloads constructor(context: Context, attrs: Attribut
     /**
      * 拖拽是否超过范围，如果超过，就不绘制拖拽圆和中间图形
      */
-    private var isOutRange: Boolean=false
+    private var isOutRange: Boolean = false
+    /**
+     * 是否全部消失
+     */
+    private var isDisappear: Boolean = false
 
 
     override fun onDraw(canvas: Canvas?) {
@@ -75,51 +73,51 @@ class DragCircleView @JvmOverloads constructor(context: Context, attrs: Attribut
         canvas?.translate(0f, -getStatusBarHeight(this).toFloat())
         //随着拖拽圆和固定圆的圆心距离越来越大，固定圆的半径越来越小
         //拖拽圆和固定圆圆心的距离百分比变化== 固定圆半径的百分比变化
-
         val distance = GeometryUtil.getDistanceBetween2Points(dragCircleCenter, stableCircleCenter)
         val percent = distance / maxDragDistance
 
-        var tempRadius = GeometryUtil.evaluateValue(percent, stableCircleRadius, dragCircleRadius)
+        var tempRadius = GeometryUtil.evaluateValue(percent,stableCircleRadius, dragCircleRadius)
         if (tempRadius < minStableRadius) {
             tempRadius = minStableRadius
         }
-        println("DragCircleView - percent=$percent  tempRadius= $tempRadius")
-        // 计算两个圆圆心的斜率
-        val dx = dragCircleCenter.x - stableCircleCenter.x
-        val dy = dragCircleCenter.y - stableCircleCenter.y
 
-        // 计算斜率
-        var lineK = 0.0
-        if (dx != 0f) {
-            lineK = ((dy / dx).toDouble())
-        }
-        // 将计算后的数据赋值给原有的静态数据
-        dragPoints = GeometryUtil.getIntersectionPoints(dragCircleCenter, dragCircleRadius, lineK).toList()
-        stablePoints = GeometryUtil.getIntersectionPoints(stableCircleCenter, stableCircleRadius, lineK).toList()
-        controlPoint = GeometryUtil.getMiddlePoint(dragCircleCenter, stableCircleCenter)
+        if (!isDisappear) {
+            if (!isOutRange) {
+                // 计算两个圆圆心的斜率
+                val dx = dragCircleCenter.x - stableCircleCenter.x
+                val dy = dragCircleCenter.y - stableCircleCenter.y
 
-        if (!isOutRange){
-            //绘制中间图形
-            val path = Path().apply {
-                //1.移动到固定圆的附着点1
-                moveTo(stablePoints[0].x, stablePoints[0].y)
-                //2.向拖拽圆附着点1绘制贝塞尔曲线
-                quadTo(controlPoint.x, controlPoint.y, dragPoints[0].x, dragPoints[0].y)
-                //3.向拖拽圆附着点2绘制直线
-                lineTo(dragPoints[1].x, dragPoints[1].y)
-                //4.向固定圆附着点2绘制贝塞尔曲线
-                quadTo(controlPoint.x, controlPoint.y, stablePoints[1].x, stablePoints[1].y)
-                //5.闭合
-                close()
+                // 计算斜率
+                var lineK = 0.0
+                if (dx != 0f) {
+                    lineK = ((dy / dx).toDouble())
+                }
+                //拖拽圆和固定圆附着点
+                var dragPoints = GeometryUtil.getIntersectionPoints(dragCircleCenter, dragCircleRadius, lineK).toList()
+                var stablePoints = GeometryUtil.getIntersectionPoints(stableCircleCenter, tempRadius, lineK).toList()
+                controlPoint = GeometryUtil.getMiddlePoint(dragCircleCenter, stableCircleCenter)
 
+                //绘制中间图形
+                val path = Path().apply {
+                    //1.移动到固定圆的附着点1
+                    moveTo(stablePoints[0].x, stablePoints[0].y)
+                    //2.向拖拽圆附着点1绘制贝塞尔曲线
+                    quadTo(controlPoint.x, controlPoint.y, dragPoints[0].x, dragPoints[0].y)
+                    //3.向拖拽圆附着点2绘制直线
+                    lineTo(dragPoints[1].x, dragPoints[1].y)
+                    //4.向固定圆附着点2绘制贝塞尔曲线
+                    quadTo(controlPoint.x, controlPoint.y, stablePoints[1].x, stablePoints[1].y)
+                    //5.闭合
+                    close()
+
+                }
+                canvas?.drawPath(path, paint)
+                // 6.重置路径，防止path重叠
+                path.reset()
+                canvas?.drawCircle(stableCircleCenter.x, stableCircleCenter.y, tempRadius, paint)
             }
-            canvas?.drawPath(path, paint)
-            // 6.重置路径，防止path重叠
-            path.reset()
             canvas?.drawCircle(dragCircleCenter.x, dragCircleCenter.y, dragCircleRadius, paint)
         }
-
-        canvas?.drawCircle(stableCircleCenter.x, stableCircleCenter.y, stableCircleRadius, paint)
         canvas?.restore()
     }
 
@@ -129,6 +127,7 @@ class DragCircleView @JvmOverloads constructor(context: Context, attrs: Attribut
             MotionEvent.ACTION_DOWN -> {
                 // 重置状态，便于调试
                 isOutRange = false
+                isDisappear = false
                 // event.getX() 点击的点距离当前自定义控件左边缘的距离
                 // event.getRawX() 点击的点距离屏幕左边缘的距离
                 var downX = event.rawX
@@ -144,7 +143,7 @@ class DragCircleView @JvmOverloads constructor(context: Context, attrs: Attribut
                 // 当拖拽圆超出一定范围后，固定圆和中间图形都消失了
                 // 获取拖拽距离，判断距离是否超出
                 val distance = GeometryUtil.getDistanceBetween2Points(dragCircleCenter, stableCircleCenter)
-                if (distance>maxDragDistance){
+                if (distance > maxDragDistance) {
 //                    拖拽是否超过范围，如果超过，就不绘制拖拽圆和中间图形
                     isOutRange = true
                 }
@@ -152,6 +151,35 @@ class DragCircleView @JvmOverloads constructor(context: Context, attrs: Attribut
             }
 
             MotionEvent.ACTION_UP -> {
+                val distance = GeometryUtil.getDistanceBetween2Points(dragCircleCenter, stableCircleCenter)
+                // 判断move的时候是否在最大范围之外
+                if (isOutRange) {
+                    // 判断up的时候是否在最大范围之外
+                    if (distance > maxDragDistance) {
+                        isDisappear = true
+                    } else {
+                        dragCircleCenter.set(stableCircleCenter.x, stableCircleCenter.y)
+                    }
+                } else {
+                    // move时候未超出最大范围，up时候也在最大范围内；
+                    // 处理：让拖拽圆向固定圆圆心方向做平移运动，当到达固定圆圆心位置后，超出一段距离，在弹回。
+                    // ValueAnimator: 当写入参数后，ValueAnimator会使其中一个参数向另一个参数变化
+                    // 将up点瞬间的拖拽圆坐标记录
+                    val tempDragCenter = PointF(dragCircleCenter.x, dragCircleCenter.y)
+                    val va = ValueAnimator.ofFloat(distance, 0f)
+                    // 添加数据变化的监听
+                    va.addUpdateListener {
+                        // 获取数据变化的百分比
+                        val animatedFraction = it.animatedFraction
+                        dragCircleCenter = GeometryUtil.getPointByPercent(tempDragCenter, stableCircleCenter, animatedFraction)
+                        invalidate()
+                    }
+                    // 动画插值器：改变动画的执行效果；动画执行到目标点之后超出一段距离，然后回到原位
+                    va.interpolator = OvershootInterpolator()
+                    va.duration = 100
+                    va.start()
+                }
+                invalidate()
             }
         }
         return true
